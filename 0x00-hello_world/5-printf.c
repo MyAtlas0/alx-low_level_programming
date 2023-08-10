50f6998cb90498758a4e9165de1c0c534abb6f2b
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2023-08-10 15:55:41 -0700

    4-puts.c and 5-printf.c commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/* Main - using void as the main function */
 int main(void)
+/* Main - this is the main function used as in this case void was the fuction used */
 {
 
 /* Printf - funtion used to print input instead of puts*/
-	printf( "with proper grammar, but the outcome is a piece of art,\n" );
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 /* Return: Used to return to 0 and terminate program*/
 
 }
