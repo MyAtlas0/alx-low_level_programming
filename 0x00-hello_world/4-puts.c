93f77b0942df5156c0f3d9445b620fd19946f315
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2023-08-10 15:37:04 -0700

    /0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Main - the main fuction used was Void */
+int main(void)
+{
+
+/* Puts - Using puts to print value instead of printf*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+
+/* Return: Used to return 0 and end program */
+}
+
+/* This code outputs "Programming is like building a multilingual puzzle. */
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Main - using void as the main function */
+int main(void)
+{
+
+/* Printf - funtion used to print input instead of puts*/
+	printf( "with proper grammar, but the outcome is a piece of art,\n" );
+	return 0;
+/* Return: Used to return to 0 and terminate program*/
+
+}
