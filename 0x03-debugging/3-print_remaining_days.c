dfa8c212c8922fc8d94cf0e58357695c084b99db
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2023-08-17 16:30:16 -0700

    0x03-debugging UPDATE

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
-		if (month >= 1 && day >= 29)
+		if (month >= 1 && day >= 1)
 		{
 			day++;
 		}
@@ -24,13 +24,13 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 12 && day == 31)
+		if (month >= 12 && day >= 31)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-			printf("Day of the yiear: %d\n", day);
+			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
