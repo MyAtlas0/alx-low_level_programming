312dc824166aa6b5fd4717bef5638fbee3bdec15
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2023-08-24 08:40:50 -0700

    0x06-pointers_arrays_strings Tasks 6 and 7 completed

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,6 +13,7 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
 
 void reverse_array(int *a, int n);
 
