0683a2b642544525b3c936f4946b894622b64302
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2023-08-29 17:57:17 -0700

    0x07-pointers_arrays_strings Advanced task

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -16,6 +16,7 @@ char *_strstr(char *haystack, char *needle);
 void simple_print_buffer(char *buffer, unsigned int size);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 unsigned int _strspn(char *s, char *accept);
 
