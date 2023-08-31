3466953e39b5b21ae2ce9efc8dc31e42c6425eb0
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2023-08-31 16:03:10 -0700

    0x08-recursion mandatory task completed

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,8 +9,9 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int _sqrt_recursive(int n, int start, int end);
+int _sqrt(int n, int find);
 int is_prime_number(int n);
+int is_prime(int n, int div);
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
