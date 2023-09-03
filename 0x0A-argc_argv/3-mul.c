31e25b09785a0180b94dceec1e1a3278163f431b
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2023-09-03 16:29:54 -0700

    3-mul.c update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,18 +15,19 @@
 
 int main(int argc, char *argv[])
 {
-	int num_1 = atoi(argv[1]);
-	int num_2 = atoi(argv[2]);
-	int result = num_1 * num_2;
+	int num_1, num_2;
+	int result;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		printf("%d\n", result);
-	}
-	return (0);	
+
+	num_1 = atoi(argv[1]);
+	num_2 = atoi(argv[2]);
+	result = num_1 * num_2;
+
+	printf("%d\n", result);
+	return (0);
 }
