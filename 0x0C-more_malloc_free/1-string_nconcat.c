2ca0462ab489aa3955f76486be87c80cb088c714
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2023-09-07 19:02:10 -0700

    0x0C-more_malloc_free task 1-string_nconcat.c correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,8 +19,10 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j;
-	unsigned int len_1, len_2;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int len_1 = 0;
+	unsigned int len_2 = 0;
 	char *str;
 
 	if (s1 == NULL)
