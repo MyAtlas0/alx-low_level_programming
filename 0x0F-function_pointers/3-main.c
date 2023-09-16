b790e32097e95471c101f45f38261a0e9458b1ea
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2023-09-15 19:31:39 -0700

    debugging 0x0F-function_pointers TASK_3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,7 +13,8 @@
 
 int main(int argc, char *argv[])
 {
-	int (*calc_func)(int, int);
+	int num_1, num_2, result;
+	char *operator;
 
 	if (argc != 4)
 	{
@@ -21,13 +22,24 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	calc_func = get_op_func(argv[2]);
-	if (calc_func == NULL || argv[2][1] != '\0')
+	num_1 = atoi(argv[1]);
+	operator = argv[2];
+	num_2 = atoi(argv[3]);
+
+	if (operator[1] != '\0' || (operator[0] != '+' && operator[0] != '-' &&
+				operator[0] != '*' && operator[0] != '/' &&
+				operator[0] != '%'))
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	if ((operator[0] == '/' || operator[0] == '%') && num_2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = get_op_func(operator)(num_1, num_2);
 
-	printf("%d\n", calc_func(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", result);
 	return (0);
 }
