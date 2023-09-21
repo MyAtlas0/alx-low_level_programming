415f3eb28131e932fd2ee66fc62f8b665e964e96
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2023-09-21 15:53:19 -0700

    0x12-singly_linked_lists TASKS 0-1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_list - Function to print all elements of the list counts the nodes.
+ *
+ * @h: A pointer to the head of the linked list.
+ *
+ * Return: The number of nodes in the linked list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%lu] %s\n", strlen(h->str), h->str);
+		}
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
+}
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * list_len - Function that counts the number of elements in a linked list.
+ *
+ * @h: A pointer to the head of the linked list.
+ *
+ * Return: The number of nodes in the linked list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
+}
