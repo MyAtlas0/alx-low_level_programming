5e30c4bbdad558c5c7f4ebc798571552af035705
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2023-09-21 19:09:40 -0700

    0x12-singly_linked_lists advanced TASks 6 assembly debugging output issue

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,15 @@
 section .data
 	hello db "Hello, Holberton", 0
-	format db "%s", 0
-	newline db 10, 0
 
 section .text
 	global main
-
-extern printf
+	extern printf
 
 main:
-	push rbp
-	mov rdi, format
-	lea rsi, [hello]
+	push rdi
+	lea rdi, [hello]
 	call printf
-
-	mov rdi, format
-	lea rsi, [newline]
-	call printf
+	pop rdi
 
 	mov rax, 60
 	xor rdi, rdi
