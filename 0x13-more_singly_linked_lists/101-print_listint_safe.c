e26e8a36524ed62f9ba0b5ee533279d296eb8691
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2023-09-27 17:49:25 -0700

    0x13-more_singly_linked_lists advanced TASK 101 debugging

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,6 +12,7 @@ size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *slow_ptr = head;
 	const listint_t *fast_ptr = head;
+	int loop_detected = 0;
 	size_t node_count = 0;
 
 	if (head == NULL)
@@ -29,11 +30,23 @@ size_t print_listint_safe(const listint_t *head)
 
 		if (slow_ptr == fast_ptr)
 		{
-			printf("-> [%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+			loop_detected = 1;
 			break;
 		}
 	}
 
+	if (loop_detected)
+	{
+		fast_ptr = head;
+		while (slow_ptr != fast_ptr)
+		{
+			printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+			node_count++;
+			slow_ptr = slow_ptr->next;
+			fast_ptr = fast_ptr->next;
+		}
+		printf("-> [%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+	}
+
 	return (node_count);
 }
-
