302bd864fdfc19ba1951e0c5d1d47dfdd19f1121
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2023-10-02 19:33:42 -0700

    0x15-file_io Advanced Task 100 Update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -47,7 +47,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	if (header == NULL)
 	{
 		close_elf(o);
-		dprintf(STDERR_FILENO, "Error: Can't allocate memory\n");
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
 
@@ -56,7 +56,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	{
 		free(header);
 		close_elf(o);
-		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: %s: No such file\n", argv[1]);
 		exit(98);
 	}
 
